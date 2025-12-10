a2816ffdf843dde5f8c9cd8114bf67799d7c867b
yeeeengyu <chldlsrb08@naver.com>
2025-12-11 00:57:44 +0900

    chore: llama-cpp-python 추가

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from utils.ask import askRequest, askResponse
 APP = FastAPI()
 
-APP.middleware(
-    middleware_type='CORS'
+APP.add_middleware(
+    CORSMiddleware,
+    allow_origins= ["*"],
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*']
 )
 @APP.get("/")
 def health():
