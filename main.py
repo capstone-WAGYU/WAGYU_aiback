b9027d0504e018a5a9a71cb8e929e2eb409705e9
yeeeengyu <chldlsrb08@naver.com>
2025-12-12 01:48:46 +0900

    feature: 모델 엔드포인트 설정

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from utils.ask import askRequest, askResponse
+from llama_cpp import Llama
 APP = FastAPI()
-
 APP.add_middleware(
     CORSMiddleware,
     allow_origins= ["*"],
@@ -10,13 +10,21 @@ APP.add_middleware(
     allow_methods = ['*'],
     allow_headers = ['*']
 )
+LLM = Llama(
+    model_path="./model/qwen2.5_merged-q4_k_m.gguf", 
+    n_ctx = 4096, 
+    n_gpu_layers= -1
+)
 @APP.get("/")
 def health():
     return {"status":"ok"}
 
 @APP.post("/ai/ask", response_model=askResponse)
 async def ask(req: askRequest):
-    return askResponse(text = req.text)
+    pipeline = LLM(
+        req.text, max_tokens = 512, temperature= 0.5, top_p = 0.9
+        ); answer = pipeline['choices'][0]['text'].strip()
+    return askResponse(text = answer)
 
 @APP.get("/ai/info")
 def getinfo(userid: str, species: str, name: str, age: int, disease: str):
